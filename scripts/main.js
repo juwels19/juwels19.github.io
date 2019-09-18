48a2bb6ef25c95279bc07f0009e2db02c74d3404
Julian Curalli <julian.curalli27@gmail.com>
2019-09-17 20:05:34 -0400

    First edition complete

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+$('.w3-bar-item').on('click', function(e) {
+    if(this.hash !== '') {
+        e.preventDefault();
+        
+        const hash = this.hash;
+        
+        $('html, body').animate(
+            {
+            scrollTop: $(hash).offset().top
+        }, 
+        800
+        );
+    }
+});
